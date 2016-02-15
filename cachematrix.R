36ba33b26b74e3b515e918916be796457c7c1bb4
pilin lo <pilin lo>
2016-02-15 09:02:27 -0800

    Finish the function and its test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Data Science Assignment for R Language Week3
+## Functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+## Creates a special Matrix that contains sub functions as below
+## 1. set : set the value of the Matrix
+## 2. get : get the value of the Matrix
+## 3. setinv : set its inverse Matrix
+## 4. getinv : get its inverse Matrix
 makeCacheMatrix <- function(x = matrix()) {
-   print(x)
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Cache the inverse result of the Matrix from makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+##My test pattern
+##Create a 2x2 matrix, called test_matrix
+##    [,1] [,2]
+##[1,]  3    4
+##[2,]  5    6
+##cache_test_matrix<-makeCacheMatrix(test_matrix)
+##cacheSolve(cache_test_matrix)
+##The inverse result is correct as below
+##    [,1] [,2]
+##[1,]-3.0  2.0
+##[2,] 2.5 -1.5
